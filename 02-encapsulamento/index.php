b229c3fafc537f183066e41d0ed3f78cd1d841a8
Fábio Santos <fabiosantos@ifsul.edu.br>
2026-03-02 14:39:57 -0300

    Orientaçaõ a Objetos Encapsulamento

--- 02-encapsulamento/index.php
+++ 02-encapsulamento/index.php
@@ -1,1 +1,36 @@
 <?php
+
+
+class User
+{
+    private $name;
+    private $email;
+    public function __construct(
+        string $name = null,
+        string $email = null)
+    {
+        $this->name = $name;
+        $this->email = $email;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name = null): void
+    {
+        $this->name = $name;
+    }
+}
+
+$user = new User("Fábio Santos", "fabiosantos@gmail.com");
+var_dump($user);
+$user->setName();
+echo "Olá, {$user->getName()}!";
+/*
+$user = new User(email:"jonh@gmail.com", name:"Jonh Santos");
+var_dump($user);
+$user = new User(email:"jonh@gmail.com");
+var_dump($user);
+*/
