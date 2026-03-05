153cf6c62605ab3f551ec29c3038c9e5dea3d638
Fábio Santos <fabiosantos@ifsul.edu.br>
2026-03-05 17:04:22 -0300

    Exercício 01

--- exercicios/exercicio01/index.php
+++ exercicios/exercicio01/index.php
@@ -1,6 +1,5 @@
 <?php
 
-
 require __DIR__ . "/../../source/autoload.php";
 //require __DIR__. "/../../source/Models/User.php";
 //require __DIR__. "/../../source/Models/Product.php";
@@ -8,7 +7,10 @@ require __DIR__ . "/../../source/autoload.php";
 use source\Models\Product;
 use source\Models\User;
 
-$product = new Product();
+$product = new Product(2, "Computador", 10000);
 $user = new User();
 var_dump($product);
+$product->discount(10);
+var_dump($product);
+$product->show();
 var_dump($user);
--- source/Models/Product.php
+++ source/Models/Product.php
@@ -6,11 +6,62 @@ class Product
 {
     private $id;
     private $name;
+    private $price;
 
-    public function __construct(int $id = null, string $name = null)
+    public function __construct(
+        int $id = null,
+        string $name = null,
+        float $price = null
+    )
     {
         $this->id = $id;
         $this->name = $name;
+        $this->price = $price;
     }
 
+    public function getId (): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getName (): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getPrice (): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): void
+    {
+        $this->price = $price;
+    }
+
+    public function discount (float $discount): void
+    {
+        $this->price = $this->price - ($discount / 100 * $this->price);
+        //$this->price -= ($discount / 100 * $this->price);
+    }
+
+    public function show(): void
+    {
+        $formatPrice = number_format(
+            $this->price,
+            2,
+            ',',
+            '.');
+        echo "<h1>Nome: {$this->name} - Preço: R$ {$formatPrice}</h1>";
+    }
 }
